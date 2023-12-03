6889d6d5834d588eeaa5dd159936648b140b1204
Jackson Roberts <jacksonroberts25@gmail.com>
2023-12-03 03:08:31 -0700

    .

--- TerminalCommand.py
+++ TerminalCommand.py
@@ -1,17 +1,26 @@
 from subprocess import getoutput
 
+class AnyType(str):
+  """A special class that is always equal in not equal comparisons. Credit to pythongosssss"""
+
+  def __ne__(self, __value: object) -> bool:
+    return False
+
+
+any = AnyType("*")
+
 class Terminal:
     def __init__(self):
         pass
     
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"image": ("IMAGE",), "text": ("STRING", {"multiline": True})}}
-    RETURN_TYPES = ("IMAGE",)
+        return {"required": {"intput": (any, {}), "text": ("STRING", {"multiline": True})}}
+    RETURN_TYPES = ("STRING")
     FUNCTION = "execute"
-    def execute(self, image, text):
+    def execute(self, _, text):
         out = getoutput(f"{text}")
         print("Output from terminal command: " + out)
-        return (image, )
+        return (out, )
 
     CATEGORY = "utils"
