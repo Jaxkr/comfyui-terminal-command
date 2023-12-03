1274008827bde1b0ed8cb342cd85048ed7621ea2
Jackson Roberts <jacksonroberts25@gmail.com>
2023-12-03 03:31:38 -0700

    .

--- TerminalCommand.py
+++ TerminalCommand.py
@@ -1,17 +1,32 @@
 from subprocess import getoutput
 
+
 class Terminal:
     def __init__(self):
         pass
-    
+
     @classmethod
     def INPUT_TYPES(s):
-        return {"required": {"image": ("IMAGE",), "text": ("STRING", {"multiline": True})}}
+        return {
+            "required": {
+                "image": ("IMAGE",),
+                "cachebuster": (
+                    "INT",
+                    {
+                        "default": 0,
+                        "display": "number",
+                    },
+                ),
+                "text": ("STRING", {"multiline": True}),
+            }
+        }
+
     RETURN_TYPES = ("IMAGE",)
     FUNCTION = "execute"
-    def execute(self, image, text):
+
+    def execute(self, image, cachebuster, text):
         out = getoutput(f"{text}")
         print("Output from terminal command: " + out)
-        return (image, )
+        return (image,)
 
     CATEGORY = "utils"
